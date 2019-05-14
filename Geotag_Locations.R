2289bfd45c5ac06c2bb27658a7bf41c584d0b32d
Daniel Payten <daniel@danielpayten.com>
2019-05-14 10:42:24 +0100

    Import script initial build

---
+++ Geotag_Locations.R
@@ -0,0 +1,7 @@
+
+
+# Author: Daniel Payten
+# Email: daniel@danielpayten.com
+
+# Date: 14/5/19
+# Purpose: This script is used to match each of the stores to a postcode and attach that postcode coordinates to the dataframe
--- src/Import_Locations.R
+++ src/Import_Locations.R
@@ -1,6 +1,22 @@
+
+
 # Author: Daniel Payten
 # Email: daniel@danielpayten.com
 
 # Date: 14/5/19
 # Purpose: This script is used to import Coral and Ladbrokes store location data from the regulator
+
+######
+# Setup
+library(tidyr)
+library(tidyverse)
+
+# Information has been extracted from: https://secure.gamblingcommission.gov.uk/PublicRegister
+
+raw_store_locations = read.csv("./data/raw/premises_licence_database.csv",header = TRUE, stringsAsFactors = FALSE)
+
+# Extract only Ladbrokes or Coral stores
+store_locations = raw_store_locations %>% 
+                  dplyr::filter(stringr::str_detect(Account_Name, 'Ladbrokes|Coral'))
+
 
